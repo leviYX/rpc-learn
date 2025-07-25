cbfcc166907e060359807cb8ba2b576b43c1d3c5
liuwenqiang <wenqiang.liu@wenge.com>
2025-07-25 11:36:24 +0800

    复杂客户端拦截器，可以拦截请求和响应。

--- rpc-grpc-adv-server/src/main/java/com/levi/GrpcServer.java
+++ rpc-grpc-adv-server/src/main/java/com/levi/GrpcServer.java
@@ -1,5 +1,6 @@
 package com.levi;
 
+import com.levi.interceptor.CustomServerInterceptor;
 import com.levi.service.HelloServiceImpl;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -10,9 +11,12 @@ public class GrpcServer {
     public static void main(String[] args) throws InterruptedException, IOException {
         ServerBuilder<?> serverBuilder = ServerBuilder.forPort(9000);
         serverBuilder.addService(new HelloServiceImpl());
+        // 注册自定义拦截器
+        serverBuilder.intercept(new CustomServerInterceptor());
         Server server = serverBuilder.build();
 
         server.start();
         server.awaitTermination();
     }
 }
+
